28eba5caae890e16bb3d81819fa54cdae0524ec6
tiagolofi <jejeco75@gmail.com>
2026-05-10 00:14:17 -0300

    adicionando nova funcionalidade

---
+++ kill-port.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Matar porta ?
+kill_port() {
+    read -p "Informe a porta: " porta
+
+    if [[ -z "$porta" ]]; then
+        echo "Nenhuma porta informada."
+        return 1
+    fi
+
+    fuser -k "${porta}/tcp"
+
+    echo "Encerrando a porta $porta"
+}
